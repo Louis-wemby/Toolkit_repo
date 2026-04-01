adf59f91a5d31a2f41f127fbaaf12111e3e69faf
Louis Xiong <144047198+Louis-wemby@users.noreply.github.com>
2026-04-01 11:45:24 +0800

    Create 3.RCTD2Seu_doublet.R

---
+++ Format_Conversion/3.RCTD2Seu_doublet.R
@@ -0,0 +1,25 @@
+```
+Construct new Seurat object from RCTD object, accomodated for Doublet mode.
+```
+
+library(Seurat)
+library(Matrix)
+
+# Loading objects
+obj <- readRDS("./RCTD.rds")
+obj2 <- readRDS("./RData.rds")
+
+ct <- obj@results[["results_df"]]
+geoseu1 <- AddMetaData(obj2,metadata = ct)
+ratios <- as.data.frame(as.matrix(obj@results[["weights"]]))
+geoseu1 <- AddMetaData(geoseu1, metadata = ratios)
+comp <- as.data.frame(as.matrix(obj@results[["weights_doublet"]]))
+colnames(comp) <- paste0(colnames(comp),"_proportion")
+geoseu1 <- AddMetaData(geoseu1, metadata = comp)
+
+meta <- geoseu1@meta.data
+meta <- meta[which(!is.na(meta$spot_class)),]
+meta$orig.ident <- rownames(meta)
+
+write.csv(meta,file = "meta.csv",quote = F,row.names = F)
+saveRDS(geoseu1, file = "RCTD_doublet.celltype.rds", compress = TRUE)
