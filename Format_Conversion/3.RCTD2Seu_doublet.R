cb2e66831c0dc8c43c3db60c9d93c1a09294f77b
Louis Xiong <144047198+Louis-wemby@users.noreply.github.com>
2026-04-06 20:36:52 +0800

    Update 3.RCTD2Seu_doublet.R

--- Format_Conversion/3.RCTD2Seu_doublet.R
+++ Format_Conversion/3.RCTD2Seu_doublet.R
@@ -1,6 +1,4 @@
-```
-Construct new Seurat object from RCTD object, accomodated for Doublet mode.
-```
+### Construct new Seurat object from RCTD object, accomodated for Doublet mode.
 
 library(Seurat)
 library(Matrix)
@@ -10,16 +8,16 @@ obj <- readRDS("./RCTD.rds")
 obj2 <- readRDS("./RData.rds")
 
 ct <- obj@results[["results_df"]]
-geoseu1 <- AddMetaData(obj2,metadata = ct)
+geoseu1 <- AddMetaData(obj2, metadata = ct)
 ratios <- as.data.frame(as.matrix(obj@results[["weights"]]))
 geoseu1 <- AddMetaData(geoseu1, metadata = ratios)
 comp <- as.data.frame(as.matrix(obj@results[["weights_doublet"]]))
-colnames(comp) <- paste0(colnames(comp),"_proportion")
+colnames(comp) <- paste0(colnames(comp), "_proportion")
 geoseu1 <- AddMetaData(geoseu1, metadata = comp)
 
 meta <- geoseu1@meta.data
-meta <- meta[which(!is.na(meta$spot_class)),]
+meta <- meta[which(!is.na(meta$spot_class)), ]
 meta$orig.ident <- rownames(meta)
 
-write.csv(meta,file = "meta.csv",quote = F,row.names = F)
+write.csv(meta, file = "meta.csv", quote = FALSE, row.names = FALSE)
 saveRDS(geoseu1, file = "RCTD_doublet.celltype.rds", compress = TRUE)
