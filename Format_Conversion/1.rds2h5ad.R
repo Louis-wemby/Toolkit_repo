96a97e0545756ecb0f4370580e15a932d375e461
Louis Xiong <144047198+Louis-wemby@users.noreply.github.com>
2026-04-10 18:02:59 +0800

    Update 1.rds2h5ad.R

--- Format_Conversion/1.rds2h5ad.R
+++ Format_Conversion/1.rds2h5ad.R
@@ -7,14 +7,33 @@ genes <- as.data.frame(rownames(object), row.names = rownames(object))
 names(genes) <- "Gene"
 
 # Add Celltype information
-cells <- data.frame(CellID = colnames(object), Celltype = object@meta.data$first_type, row.names = colnames(object))
+cells <- data.frame(CellID = colnames(object),
+                    Celltype = object@meta.data$first_type,
+                    row.names = colnames(object))
+
+# If your Seurat obj looks like this:
+# Active assay: Spatial
+#  1 image present: slice1
 row <- object@images[["slice1"]]@coordinates$row
 col <- object@images[["slice1"]]@coordinates$col
 coordinates <- list(matrix(c(row, col), ncol = 2))
 names(coordinates) <- "spatial"
 
+# If your Seurat obj looks like this:
+# Active assay: RNA
+#  1 dimensional reduction calculated: spatial
+# Use the code below:
+# coords <- as.data.frame(object[["spatial"]]@cell.embeddings)
+# row <- coords$SPATIAL_1
+# col <- coords$SPATIAL_2
+# coordinates <- list(matrix(c(row, col), ncol = 2))
+
 anndata <- import("anndata", delay_load = TRUE)
-ad <- anndata$AnnData(X = object@assays$Spatial@counts, obs = genes, var = cells, varm = coordinates)
+ad <- anndata$AnnData(X = object@assays$Spatial@counts,
+                      # or `X = object@assays$RNA@counts`, respectively
+                      obs = genes,
+                      var = cells,
+                      varm = coordinates)
 
 ad <- ad$T
 ad$write_h5ad(file.path("AnnData.h5ad"))
